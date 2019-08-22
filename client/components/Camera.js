671d4724d46fc6693a69effd87ab36ba39bf0793
Amir Keren <amir.k@taboola.com>
2019-08-23 00:12:23 +0300

    Playing with the model

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -1,13 +1,14 @@
-import {drawKeyPoints, drawSkeleton} from './utils'
-import React, {Component} from 'react'
-import * as posenet from '@tensorflow-models/posenet'
+import { drawKeyPoints, drawSkeleton } from "./utils";
+import React, { Component } from "react";
+import * as posenet from "@tensorflow-models/posenet";
+import PropTypes from "prop-types";
 
 class PoseNet extends Component {
   static defaultProps = {
     videoWidth: 900,
     videoHeight: 700,
     flipHorizontal: true,
-    algorithm: 'single-pose',
+    algorithm: "single-pose",
     showVideo: true,
     showSkeleton: true,
     showPoints: true,
@@ -17,147 +18,144 @@ class PoseNet extends Component {
     nmsRadius: 20,
     outputStride: 16,
     imageScaleFactor: 0.5,
-    skeletonColor: '#ffadea',
+    skeletonColor: "#ffadea",
     skeletonLineWidth: 6,
-    loadingText: 'Loading...please be patient...'
-  }
+    loadingText: "Loading...please be patient..."
+  };
 
   constructor(props) {
-    super(props, PoseNet.defaultProps)
+    super(props, PoseNet.defaultProps);
   }
 
   getCanvas = elem => {
-    this.canvas = elem
-  }
+    this.canvas = elem;
+  };
 
   getVideo = elem => {
-    this.video = elem
-  }
+    this.video = elem;
+  };
 
   async componentDidMount() {
     try {
-      await this.setupCamera()
+      await this.setupCamera();
     } catch (error) {
       throw new Error(
-        'This browser does not support video capture, or this device does not have a camera'
-      )
+        "This browser does not support video capture, or this device does not have a camera"
+      );
     }
 
     try {
-      this.posenet = await posenet.load()
+      // this.posenet = await posenet.load();
+      this.posenet = await posenet.load({
+        // architecture: 'ResNet50',
+        // outputStride: 16,
+        // inputResolution: 801,
+        // quantBytes: 4
+        architecture: "MobileNetV1",
+        outputStride: 16,
+        inputResolution: 161,
+        multiplier: 0.5
+      });
     } catch (error) {
-      throw new Error('PoseNet failed to load')
+      throw new Error("PoseNet failed to load");
     } finally {
       setTimeout(() => {
-        this.setState({loading: false})
-      }, 200)
+        this.setState({ loading: false });
+      }, 200);
     }
-
-    this.detectPose()
+    this.detectPose();
   }
 
   async setupCamera() {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
       throw new Error(
-        'Browser API navigator.mediaDevices.getUserMedia not available'
-      )
+        "Browser API navigator.mediaDevices.getUserMedia not available"
+      );
     }
-    const {videoWidth, videoHeight} = this.props
-    const video = this.video
-    video.width = videoWidth
-    video.height = videoHeight
-
-    const stream = await navigator.mediaDevices.getUserMedia({
+    const { videoWidth, videoHeight } = this.props;
+    const video = this.video;
+    video.width = videoWidth;
+    video.height = videoHeight;
+    video.srcObject = await navigator.mediaDevices.getUserMedia({
       audio: false,
       video: {
-        facingMode: 'user',
+        facingMode: "user",
         width: videoWidth,
         height: videoHeight
       }
-    })
-
-    video.srcObject = stream
-
+    });
     return new Promise(resolve => {
       video.onloadedmetadata = () => {
-        video.play()
-        resolve(video)
-      }
-    })
+        video.play();
+        resolve(video);
+      };
+    });
   }
 
   detectPose() {
-    const {videoWidth, videoHeight} = this.props
-    const canvas = this.canvas
-    const canvasContext = canvas.getContext('2d')
-
-    canvas.width = videoWidth
-    canvas.height = videoHeight
-
-    this.poseDetectionFrame(canvasContext)
+    const { videoWidth, videoHeight } = this.props;
+    const canvas = this.canvas;
+    const canvasContext = canvas.getContext("2d");
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+    this.poseDetectionFrame(canvasContext);
   }
 
   poseDetectionFrame(canvasContext) {
     const {
       algorithm,
-      imageScaleFactor, 
-      flipHorizontal, 
-      outputStride, 
-      minPoseConfidence, 
-      minPartConfidence, 
-      maxPoseDetections, 
-      nmsRadius, 
-      videoWidth, 
-      videoHeight, 
-      showVideo, 
-      showPoints, 
-      showSkeleton, 
-      skeletonColor, 
-      skeletonLineWidth 
-      } = this.props
-
-    const posenetModel = this.posenet
-    const video = this.video
-
+      imageScaleFactor,
+      flipHorizontal,
+      outputStride,
+      minPoseConfidence,
+      minPartConfidence,
+      maxPoseDetections,
+      nmsRadius,
+      videoWidth,
+      videoHeight,
+      showVideo,
+      showPoints,
+      showSkeleton,
+      skeletonColor,
+      skeletonLineWidth
+    } = this.props;
+    const posenetModel = this.posenet;
+    const video = this.video;
     const findPoseDetectionFrame = async () => {
-      let poses = []
-
+      let poses = [];
       switch (algorithm) {
-        case 'multi-pose': {
+        case "multi-pose": {
           poses = await posenetModel.estimateMultiplePoses(
-          video, 
-          imageScaleFactor, 
-          flipHorizontal, 
-          outputStride, 
-          maxPoseDetections, 
-          minPartConfidence, 
-          nmsRadius
-          )
-          break
+            video,
+            imageScaleFactor,
+            flipHorizontal,
+            outputStride,
+            maxPoseDetections,
+            minPartConfidence,
+            nmsRadius
+          );
+          break;
         }
-        case 'single-pose': {
+        case "single-pose": {
           const pose = await posenetModel.estimateSinglePose(
-          video, 
-          imageScaleFactor, 
-          flipHorizontal, 
-          outputStride
-          )
-          poses.push(pose)
-          break
+            video,
+            imageScaleFactor,
+            flipHorizontal,
+            outputStride
+          );
+          poses.push(pose);
+          break;
         }
       }
-
-      canvasContext.clearRect(0, 0, videoWidth, videoHeight)
-
+      canvasContext.clearRect(0, 0, videoWidth, videoHeight);
       if (showVideo) {
-        canvasContext.save()
-        canvasContext.scale(-1, 1)
-        canvasContext.translate(-videoWidth, 0)
-        canvasContext.drawImage(video, 0, 0, videoWidth, videoHeight)
-        canvasContext.restore()
+        canvasContext.save();
+        canvasContext.scale(-1, 1);
+        canvasContext.translate(-videoWidth, 0);
+        canvasContext.drawImage(video, 0, 0, videoWidth, videoHeight);
+        canvasContext.restore();
       }
-
-      poses.forEach(({score, keypoints}) => {
+      poses.forEach(({ score, keypoints }) => {
         if (score >= minPoseConfidence) {
           if (showPoints) {
             drawKeyPoints(
@@ -165,7 +163,7 @@ class PoseNet extends Component {
               minPartConfidence,
               skeletonColor,
               canvasContext
-            )
+            );
           }
           if (showSkeleton) {
             drawSkeleton(
@@ -174,13 +172,13 @@ class PoseNet extends Component {
               skeletonColor,
               skeletonLineWidth,
               canvasContext
-            )
+            );
           }
         }
-      })
-      requestAnimationFrame(findPoseDetectionFrame)
-    }
-    findPoseDetectionFrame()
+      });
+      requestAnimationFrame(findPoseDetectionFrame);
+    };
+    findPoseDetectionFrame();
   }
 
   render() {
@@ -191,8 +189,26 @@ class PoseNet extends Component {
           <canvas className="webcam" ref={this.getCanvas} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default PoseNet
+PoseNet.propTypes = {
+  algorithm: PropTypes.string,
+  imageScaleFactor: PropTypes.number,
+  flipHorizontal: PropTypes.bool,
+  outputStride: PropTypes.number,
+  minPoseConfidence: PropTypes.number,
+  minPartConfidence: PropTypes.number,
+  maxPoseDetections: PropTypes.number,
+  nmsRadius: PropTypes.number,
+  videoWidth: PropTypes.number,
+  videoHeight: PropTypes.number,
+  showVideo: PropTypes.bool,
+  showPoints: PropTypes.bool,
+  showSkeleton: PropTypes.bool,
+  skeletonColor: PropTypes.string,
+  skeletonLineWidth: PropTypes.number
+};
+
+export default PoseNet;
